c85e86f79c64d4e79327606b6e335846ade31aec
HennsenA <3030119040108@ingenieria.usac.edu.gt>
2026-08-11 18:28:56 -0600

    Creacion del proyecto y su clase principal "Sistema"

---
+++ IPC2_Proy01_202602_202500286/Program.cs
@@ -0,0 +1,26 @@
+﻿public class Sistema { 
+    public static void main(string[] args)
+    {
+        int option = 1;
+        while (option > 0) {
+            mostrarMenu();
+            option = int.Parse(Console.ReadLine());
+        }
+    }
+
+    public static void mostrarMenu() {
+        String menu =
+            """
+            =================================================
+            |           Chapin Wariors System               |
+            =================================================
+            |   0. Salir                                    |
+            |   1. Cargar archivos de configuracion         |
+            |   2. Ejecutar Misiones                        |
+            =================================================
+            |Selecciona una opcion:
+            """;
+
+        Console.WriteLine(menu);
+    }
+}
