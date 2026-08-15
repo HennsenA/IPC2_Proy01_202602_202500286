a4e1f158fb6c19aec033f7475f2f6e528fdedaae
HennsenA <3030119040108@ingenieria.usac.edu.gt>
2026-08-14 22:46:00 -0600

    estructura basica de la clase ManejoXML

--- IPC2_Proy01_202602_202500286/XML/ManejoXML.cs
+++ IPC2_Proy01_202602_202500286/XML/ManejoXML.cs
@@ -1,11 +1,23 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Xml.Linq;
 
 namespace IPC2_Proy01_202602_202500286.XML
 {
     public class ManejoXML //Clase que maneja el archivo xml y crea los objetos Robot y Ciudad
     {
+        public void CargarArchivo()
+        {
 
+        }
+        public void CargarCiudades()
+        {
+
+        }
+        public void CargarRobots()
+        {
+
+        }
     }
 }
